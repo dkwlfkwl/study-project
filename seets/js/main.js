786068c57cee85834c75f94a0198156f90e7369f
Dada kim <user@DESKTOP-S14NTN7.(none)>
2019-03-05 22:03:39 +0900

    다영 3/5

--- seets/js/main.js
+++ seets/js/main.js
@@ -1,6 +1,7 @@
 
 $(function(){
 
+	//패키지안내
 	$('.tour_list > li > a').on('click', function(){
 		var ts = $(this);
 		var idxNum = $('.tour_list > li > a').index(ts);
@@ -19,6 +20,56 @@ $(function(){
 	$('.tour_con li').on('mouseleave', function(){
 		$(this).children().stop().css({'top':'-100%'});
 	});
+
+	//광고
+	$('.advertising_list > li > a').on('click', function(){
+		var ts = $(this);
+		var idxNum = $('.advertising_list > li > a').index(ts);
+		if(!ts.hasClass('on')){
+			$('.advertising_list > li > a').filter('.on').removeClass('on');
+			ts.addClass('on');
+			
+		$('.advertising_con').filter('.on').removeClass('on');
+		$('.advertising_wrap .advertising_con').eq(idxNum).addClass('on');
+		}
+	});
+
+	//이벤트
+	$('.event_slide_wrap .slide_wrap').slick({
+		autoplay: true,
+		autoplaySpeed:2000,
+		dots: true,
+		arrows:false,
+		infinite: true,
+		speed: 300
+	});
+	
+	//베스트 후기
+		
+	$('.review_slide_wrap .slide_wrap').slick({
+	  centerMode: true,
+	  //centerPadding: '50px',
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrow:true,
+	  //dots: true,
+	  infinite: true,
+	  autoplay: true,
+	  autoplaySpeed: 2500,
+	  responsive: [
+		{
+		  breakpoint: 768,
+		  settings: {
+			slidesToShow: 1
+		  }
+		}
+	  ]
+	});
+	
+	/* count */
+	var $gallery = $('.review_slide_wrap .slide_wrap');
+	var slideCount = null;
+
 
 	
 	
