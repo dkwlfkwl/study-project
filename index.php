bbbd9a125764f0728b57ee8116c4f55790ff81b2
unknown <ATIV@ATIV9.(none)>
2019-03-05 21:04:15 +0900

    php

---
+++ index.php
@@ -0,0 +1,254 @@
+<?php include_once("seets/page/top.html");?>
+  <title>Travel | Home</title>
+	<script src="seets/js/main.js"></script>
+	<link rel="stylesheet" href="seets/css/main.css" class="css">
+ </head>
+ <body>
+	<div class="skip">
+		<a href="#header">상단 바로가기</a>
+		<a href="#contents">본문 바로가기</a>
+		<a href="#footer">하단 바로가기</a>
+	</div>
+  <div id="wrap">
+<?php include_once("seets/page/header.html");?>
+		<div id="body_wrap" class="w1920">
+			<section id="main_visual_wrap">
+				<h1 class="blind">메인비주얼</h1>
+				<div class="visual"></div>
+				<div class="txt_area tly-50 tac">
+					<div class="txt_wrap white">
+						<p class="txt45 fw300">여행의 시작</p>
+						<p class="txt100 fw700">Travel</p>
+						<p class="txt18 fw100 wk mt25">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+			</section>
+			<section id="container" class="m_con">
+				<h1 class="blind">본문</h1>
+				<article class="con con1">
+					<h2 class="blind">예약/일정</h2>
+					<div class="w1200 cfix">
+						<div class="right"></div>
+						<div class="left"></div>
+					</div>
+				</article>
+				<article class="con con2">
+					<div class="w1200">
+						<h2 class="m_tit txt24 orange fw700">패키지 안내</h2>
+						<div class="tour_top">
+							<ul class="tour_list mt20 cfix">
+								<li><a href="#;" class="txt16 tac on">터키</a></li>
+								<li><a href="#;" class="txt16 tac">제주</a></li>
+								<li><a href="#;" class="txt16 tac">전라도</a></li>
+								<li><a href="#;" class="txt16 tac">경상도</a></li>
+								<li><a href="#;" class="txt16 tac">강원도</a></li>
+								<li><a href="#;" class="txt16 tac">유럽</a></li>
+								<li><a href="#;" class="txt16 tac">동남아</a></li>
+								<li><a href="#;" class="txt16 tac">중국</a></li>
+								<li><a href="#;" class="txt16 tac">대만</a></li>
+								<li><a href="#;" class="txt16 tac">홍콩</a></li>
+								<li><a href="#;" class="txt16 tac">괌/사이판</a></li>
+								<li><a href="#;" class="txt16 tac">호주/뉴질랜드</a></li>
+								<li><a href="#;" class="txt16 tac">미국</a></li>
+							</ul>
+							<a href="#;" class="btn_more red">+ more</a>
+						</div>
+						<div class="tour_con_wrap mt30">
+							<div class="tour_con on">
+								<ul class="cfix">
+									<li>
+										<a href="#;">
+											<p class="img"><img src="ssets/img/main/tour_0_1.jpg" alt="터키 투어" class="tl-50"></p>
+											<div class="txt_wrap tac">
+												<p class="txt20 fw700 ellipsis">볼수록 매력적인 터키 여행!</p>
+												<p class="txt txt14 ellipsis">터키 6대 관광지 완전 일주 / 도마바흐체 궁전 포함 </p>
+											</div>
+										</a>
+										<div class="txt_area">
+											<p class="txt20 tl-50"><a href="#">View</a></p>
+										</div>
+									</li>
+									<li>
+										<a href="#;">
+											<p class="img"><img src="ssets/img/main/tour_0_2.jpg" alt="터키 투어" class="tl-50"></p>
+											<div class="txt_wrap tac">
+												<p class="txt20 fw700 ellipsis">터키 일주 9일</p>
+												<p class="txt txt14 ellipsis">이스탄불 보스포러스 해협 크루즈 전세선 탑승 포함</p>
+											</div>
+										</a>
+										<div class="txt_area">
+											<p class="txt20 tl-50"><a href="#">View</a></p>
+										</div>
+									</li>
+									<li>
+										<a href="#;">
+											<p class="img"><img src="ssets/img/main/tour_0_3.jpg" alt="터키 투어" class="tl-50"></p>
+											<div class="txt_wrap tac">
+												<p class="txt20 fw700 ellipsis">알차케 떠나는 터키, 초특가 패키지</p>
+												<p class="txt txt14 ellipsis">전일정식사포함 / 터키 6대 관광지 완전 일주</p>
+											</div>
+										</a>
+										<div class="txt_area">
+											<p class="txt20 tl-50"><a href="#">View</a></p>
+										</div>
+									</li>
+								</ul>
+							</div>
+							<div class="tour_con">
+								<ul class="cfix">
+									<li>
+										<a href="#;">
+											<p class="img"><img src="ssets/img/main/tour_1_1.jpg" alt="제주 투어" class="tl-50"></p>
+											<div class="txt_wrap tac">
+												<p class="txt20 fw700 ellipsis">탐나는 제주 2박 3일</p>
+												<p class="txt txt14">전일정식사포함 / 잠수함 / 마라도 / 우도</p>
+											</div>
+										</a>
+										<div class="txt_area">
+											<p class="txt20 tl-50"><a href="#">View</a></p>
+										</div>
+									</li>
+									<li>
+										<a href="#;">
+											<p class="img"><img src="ssets/img/main/tour_1_2.jpg" alt="제주 투어" class="tl-50"></p>
+											<div class="txt_wrap tac">
+												<p class="txt20 fw700 ellipsis">특가로 알차게 떠나는 제주여행</p>
+												<p class="txt txt14">전일정식사포함 / 천지연폭포 / 새별오름 / 감귤따기</p>
+											</div>
+										</a>
+										<div class="txt_area">
+											<p class="txt20 tl-50"><a href="#">View</a></p>
+										</div>
+									</li>
+									<li>
+										<a href="#;">
+											<p class="img"><img src="ssets/img/main/tour_1_3.jpg" alt="제주 투어" class="tl-50"></p>
+											<div class="txt_wrap tac">
+												<p class="txt20 fw700 ellipsis">한라산 눈꽃 등반 초특가 패키지</p>
+												<p class="txt txt14">전일정식사포함 / 한라산등반 / 억새풀투어</p>
+											</div>
+										</a>
+										<div class="txt_area">
+											<p class="txt20 tl-50"><a href="#">View</a></p>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</article>
+				<article class="con con3">
+					<div class="w1200 cfix">
+						<h2 class="m_tit txt24 orange fw700">광고 & 이벤트</h2>
+						<div class="left">
+							<ul class="advertising_list">
+								<li><a href="#;" class="on right">T V</a></li>
+								<li><a href="#;" class="right">C F</a></li>
+								<li><a href="#;" class="right">홈쇼핑</a></li>
+							</ul>
+							<div class="advertising_wrap">
+								<div class="advertising_con on">
+									<div class="video_wrap">
+										<iframe src="https://www.youtube.com/embed/7Db8F0Kqcfw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+									</div>
+								</div>
+								<div class="advertising_con">
+									<div class="video_wrap">
+										<iframe src="https://www.youtube.com/embed/7Db8F0Kqcfw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+									</div>
+								</div>
+								<div class="advertising_con">
+									<div class="video_wrap">
+										<iframe src="https://www.youtube.com/embed/7Db8F0Kqcfw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="right">
+							<div class="event_slide_wrap">
+								<ul class="slide_wrap">
+									<li><img src="seets/img/main/event_1.jpg" alt="이벤트"></li>
+									<li><img src="seets/img/main/event_1.jpg" alt="이벤트"></li>
+									<li><img src="seets/img/main/event_1.jpg" alt="이벤트"></li>
+									<li><img src="seets/img/main/event_1.jpg" alt="이벤트"></li>
+									<li><img src="seets/img/main/event_1.jpg" alt="이벤트"></li>
+									<li><img src="seets/img/main/event_1.jpg" alt="이벤트"></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</article>
+				<article class="con con4">
+					<div class="w1200">
+						<h2 class="m_tit txt24 orange fw700">베스트 여행후기</h2>
+						<div class="review_slide_wrap">
+							<ul class="slide_wrap">
+								<li>
+									<p class="img"><img src="seets/img/main/review_1.jpg" alt="VR 여행후기"></p>
+									<div class="txt_area">
+										<div class="txt_wrap">
+											<p class="txt txt18 black tac">11111</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<p class="img"><img src="seets/img/main/review_2.jpg" alt="VR 여행후기"></p>
+									<div class="txt_area">
+										<div class="txt_wrap">
+											<p class="txt txt18 black tac">11111</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<p class="img"><img src="seets/img/main/review_3.jpg" alt="VR 여행후기"></p>
+									<div class="txt_area">
+										<div class="txt_wrap">
+											<p class="txt txt18 black tac">11111</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<p class="img"><img src="seets/img/main/review_4.jpg" alt="VR 여행후기"></p>
+									<div class="txt_area">
+										<div class="txt_wrap">
+											<p class="txt txt18 black tac">11111</p>
+										</div>
+									</div>
+								</li>
+							</ul>
+							<div class="slide-count-wrap  txt14">
+								 <span class="current"></span> /
+								 <span class="total"></span>
+							</div> 
+						</div>
+					</div>
+				</article>
+				<article class="con con5">
+					<h2 class="blind">공지사항 & 자주하는질문</h2>
+					<div class="w1200 cfix">
+						<div class="left">
+							<h3 class="m_tit txt24 orange fw700">공지사항</h2>
+
+						</div>
+						<div class="right">
+							<h3 class="m_tit txt24 orange fw700">자주하는 질문<p></p></h2>
+
+						</div>
+					</div>
+				</article>
+			</section>
+		</div>
+		<footer id="footer">
+			<div class="w1920">
+				<ul class="mb20">
+					<li class="first"><a href="#;" class="txt14">회사소개</a></li>
+					<li><a href="#;" class="txt14">개인정보취급방침</a></li>
+					<li><a href="#;" class="txt14">이용약관</a></li>
+					<li><a href="#;" class="txt14">이메일무단수집거부</a></li>
+				</ul>
+				<p class="txt12">copyright&copy; Travel All Rights Reserved.</p>
+			</div>
+		</footer>
+	</div>
+ </body>
+</html>
